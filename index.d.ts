178fa102d9d3c646bc76b07529d8cc5385e7704e
Jan Nitschke <jan.nitschke@gmail.com>
2019-10-14 11:07:23 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,30 @@
-declare module vdbststd{
+export as namespace std;
 
-    export class Result<T>{
+export class Result<T>{
 
-        constructor(state: string, value: T): Result<T>
-    
-        is_err(): boolean;
-    
-        is_ok(): boolean;
-    
-        or_Fail(): T;
-        
-        or(handler): Result<T>;
-        
-        and(handler): Result<T>;
-    
-        unwrap(): T | undefined;    
+    constructor(state: string, value: T): Result<T>
 
-        static Ok<T>(result: T): Result<T>;
+    is_err(): boolean;
+
+    is_ok(): boolean;
+
+    or_Fail(): T;
     
-        static Err<T>(reason: any): Result<T>;
-    }
-
-    export function match(subject: any, options: any): any;
-
-    export function Ok<T>(result: T): Result<T>
+    or(handler): Result<T>;
     
-    export function Err(err: any): Result<null>
+    and(handler): Result<T>;
 
-    export function fn<T>(unpatched: () => T) : Result<T>;
-}0
+    unwrap(): T | undefined;    
+
+    static Ok<T>(result: T): Result<T>;
+
+    static Err<T>(reason: any): Result<T>;
+}
+
+export function match(subject: any, options: any): any;
+
+export function Ok<T>(result: T): Result<T>
+
+export function Err(err: any): Result<null>
+
+export function fn<T>(unpatched: () => T) : Result<T>;
